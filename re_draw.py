2cbc931d0f058f19aab12a3cfb682b4381f09912
Tran Du <duvinhhb@gmail.com>
2024-10-16 16:20:08 +0700

    update

--- re_draw.py
+++ re_draw.py
@@ -78,6 +78,7 @@ def plot_sensor(sensor_nodes, fitness):
     caculator = len(ban_position)/((VarMaxy+1)*(VarMaxx+1))
     ax.set_title(f"Coverage percent:{round(fitness/(1-caculator),4) * 100 :.2f} %")
     ay.set_title(f"Coverage percent:{round(fitness/(1-caculator),4) * 100 :.2f} %")
+    plt.savefig('graph.pdf', format='pdf', dpi=300)
     plt.show()
 
 with open('FOA.CSV', mode ='r', newline='') as file:
